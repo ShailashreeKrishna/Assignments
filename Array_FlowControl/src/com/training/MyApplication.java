b6595dc728fa7520265a553d3919b03fa3ff8edd
Shailashree.S <Shailashree.S@EXIND3020-76.allfon.in>
2022-04-07 11:43:15 +0530

    Second Commit

---
+++ Array_FlowControl/src/com/training/MyApplication.java
@@ -0,0 +1,46 @@
+package com.training;
+import com.training.model.BankAccount;
+import com.training.services.BankAccountService;
+import java.util.*;
+public class MyApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAccountService service = new BankAccountService();
+		BankAccount[] list=	new BankAccount[1];
+		int idx=0;
+		int choice=1;
+				Scanner sc = new Scanner(System.in);
+		do {
+		
+		
+		System.out.println("Enter the accountnumber:");
+		int accountNumber = sc.nextInt();
+		
+		
+		System.out.println("enter holdername:");
+		String accountHolderName = sc.next();
+		
+		System.out.println("enter balance");
+		double balance=sc.nextDouble();
+		System.out.println("Account Type[savings,fixed,recurring]");
+		String accountType=sc.next();
+		System.out.println("Enter 0 to end 1 continue");
+		choice=sc.nextInt();
+		BankAccount account = new BankAccount(accountNumber,accountHolderName,balance,accountType);
+		
+		list[idx]=account;
+		
+		
+		
+		}while(choice==1);
+			
+			double[] values = service.findInterestByAccountType(list);
+			for(double eachValue: values) {
+				System.out.println(eachValue);
+				sc.close();
+			}
+			
+
+}
+}
---
+++ Array_FlowControl/src/com/training/services/Application.java
@@ -0,0 +1,38 @@
+package com.training.services;
+import com.training.model.*;
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 BankAccountService service = new BankAccountService();
+		 BankAccount ram = new BankAccount(1200,"Ramesh",5000);
+		 double simpleInterest = service.calculateInterest(ram);
+		 System.out.println("Simple interest:"+simpleInterest);
+		 BankAccount shyam = new BankAccount(6200,"Rajesh",5000);
+		 double simpleInterest2 = service.calculateInterest(shyam);
+		 System.out.println("SimpleInterest:"+simpleInterest2);
+//		 BankAccount[] accounts = new BankAccount[2];
+//		 accounts[0]=ram;
+//		 accounts[1]=shyam;
+//		 service.calculateInterest(accounts);
+		 BankAccount[] accounts = {ram,shyam};
+		 service.calculateInterest(accounts);
+		 System.out.println("======Array==========");
+		 
+		 double[] values = service.findInterest(accounts);
+		 for(double eachValue: values) {
+			 System.out.println(eachValue);
+		 }
+		 
+		BankAccount[] accountList = {
+				new BankAccount(800,"jay",5000,"savings"),
+				new BankAccount(801,"kam",6000,"fixed"),
+				new BankAccount(802,"tom",7000,"recurring")
+		};
+		 double[] interestValues = service.findInterestByAccountType(accountList);
+         
+		 for(double eachvalue: interestValues) {
+			 System.out.println(eachvalue);
+		 }
+	}
+}

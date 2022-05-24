c1c05ec4b1e9a1a37747fd345c4e936c7dce2791
Shailashree.S <Shailashree.S@EXIND3020-76.allfon.in>
2022-05-24 16:45:19 +0530

    actuator

---
+++ Spring_Boot_Actuator/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Generated;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api/v1")
+@Slf4j
+public class BookController {
+	
+	
+	@GetMapping(path="/books")
+	
+	public List<String> getBooks(){
+		log.info("Get books called");
+		return Arrays.asList("Head First Java","Spring in Action","Thinking in Java");
+	}
+	
+	@GetMapping(path="/books/{id}")
+	public String getBookById(@PathVariable int id) {
+		
+		if(id==1) {
+			return "Head First Java";
+		} else {
+			return "Spring in Action";
+		}
+	}
+	
+
+}

6335fe92692e72673fb886ec7fb4706341cd3b30
Shailashree.S <Shailashree.S@EXIND3020-76.allfon.in>
2022-08-10 09:45:19 +0530

    catalog service client

---
+++ catalog-service-client/src/main/java/com/example/demo/controller/ClientController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@RestController
+public class ClientController {
+	
+	@Autowired 
+	private RestTemplate template;
+	
+	
+	@GetMapping(path="/client")
+	
+    @CircuitBreaker(name="catalog",fallbackMethod="getCatalogFallBack")
+	 public String getCatalog() {
+		
+		
+		return template.getForObject("http://localhost:2020/api/v1/catalogs",String.class);
+	}
+	
+//terminate the service then run this we get from fall back as result
+	public String getCatalogFallBack(Exception e) {
+//		
+//		e.printStackTrace();
+//		
+//		return "From Fall Back";
+		
+		return ("{charge : 12 per km}");
+	}
+}
